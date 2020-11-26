ee2ace94c39673349e6649d5b8543946a124bd9e
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-11-26 14:55:59 +0000

    input from user sterilized with parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,22 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const query = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teacher;;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teacher
+`;
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+
+
+pool.query(query,values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher} `);
